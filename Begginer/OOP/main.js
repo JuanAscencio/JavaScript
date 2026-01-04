27faf85e87fd77c5f1656f2f3f527afe59846431
Juan Ascencio <juan.ascencio.tapia@gmail.com>
2026-01-04 17:23:45 -0600

    POO

--- Begginer/OOP/main.js
+++ Begginer/OOP/main.js
@@ -1,1 +1,60 @@
-console.log("xD")
+// Constructor function
+function Person(firstName, lastName, dob){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+/*
+    // Methods - same as prototype
+    // It's not in the object it's in the prototype
+    this.getBirthYear = function(){
+        return this.dob.getFullYear()
+    }
+
+    this.getFullName = function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+*/
+}
+
+// Prototype
+Person.prototype.getBirthYear = function(){
+    return this.dob.getFullYear()
+}
+Person.prototype.getFullName = function(){
+    return `${this.firstName} ${this.lastName}`
+}
+
+
+/*
+// Class - sintactic sugar
+class Person{
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
+
+    getBirthYear(){
+        return this.dob.getFullYear()
+    }
+
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
+    }
+
+}
+*/
+
+
+// Instantiate object
+const person1 = new Person("John", "Doe", "4-3-1980");
+const person2 = new Person("Mary", "Smith", "3-6-1970");
+
+console.log(person1);
+console.log(person2);
+
+console.log(`The year of birth is: ${person1.getBirthYear()}`);
+console.log(`The year of birth is: ${person2.getBirthYear()}`);
+
+console.log(`Person 1. Full name: ${person1.getFullName()}`);
+console.log(`Person 2. Full name: ${person2.getFullName()}`);
